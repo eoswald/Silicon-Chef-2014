1e7d5fe0e415ad7d6f32ee999197b9340eb06f0b
Eric Oswald <eoswald39@gmail.com>
2014-11-01 15:09:28 -0400

    Test drive code

--- car/src/sketch.ino
+++ car/src/sketch.ino
@@ -4,21 +4,7 @@
 #define BOTTOM_RIGHT 5
 #define PWM_PIN 6
 
-#define NUM_BLOCKS 10
 #define SPEED 100
-
-#define BRICK_VAL
-#define WOOD_VAL
-#define ALUMINUM_VAL
-#define STEEL_VAL
-
-#define LIGHT_POWER 7
-#define LIGHT_SENSOR A0
-#define SAMPLES 50
-
-int block_count = 10;
-int blocks[10];
-int nextBlockVal;
 
 void setup() {
 	pinMode(TOP_LEFT, OUTPUT);
@@ -29,61 +15,10 @@ void setup() {
 }
 
 void loop() {
-	while (block_count < NUM_BLOCKS) {
-		forward(SPEED);
-		while (not isBlock());
-		block_count++;
-		blocks[block_count] = blockVal();
-	}
-	calcNextBlock();
-}
-
-void calcNextBlock(){
-	for (int i = 0; i < 10; ++i)
-		if (blocks[i] >= BRICK_VAL) {
-			nextBlockVal = BRICK_VAL;
-			return;
-		}
-	}
-	for (int i = 0; i < 10; ++i)
-		if (blocks[i] >= WOOD_VAL) {
-			nextBlockVal = WOOD_VAL;
-			return;
-		}
-	}
-	for (int i = 0; i < 10; ++i)
-		if (blocks[i] >= ALUMINUM_VAL) {
-			nextBlockVal = ALUMINUM_VAL;
-			return;
-		}
-	}
-	for (int i = 0; i < 10; ++i)
-		if (blocks[i] >= STEEL_VAL) {
-			nextBlockVal = STEEL_VAL;
-			return;
-		}
-	}
-}
-
-// returns true if we are on top of a block
-bool isBlock() {
-}
-
-// reads the optical measurement of block
-int blockVal() {
-	long temp = 0;
-	unsigned int hi;
-	for (unsigned int i = 0; i < SAMPLES; i++)
-	{
-		digitalWrite(LIGHT_POWER, HIGH);
-		delay(1);
-		hi = analogRead(LIGHT_SENSOR);
-		digitalWrite(LIGHT_POWER, LOW);
-		delay(1);
-		temp += analogRead(LIGHT_SENSOR) - hi;
-	}
-	temp /= SAMPLES;
-	return (int) temp;
+	forward(SPEED);
+	delay(3000);
+	reverse(SPEED);
+	delay(3000);
 }
 
 void forward(int speed) {
