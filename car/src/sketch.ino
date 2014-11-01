746ce75be39f431579f5c80462153f8844ea4b04
Eric Oswald <eoswald39@gmail.com>
2014-11-01 14:28:57 -0400

    Added driving code

--- car/src/sketch.ino
+++ car/src/sketch.ino
@@ -4,6 +4,18 @@
 #define BOTTOM_RIGHT 5
 #define PWM_PIN 6
 
+#define NUM_BLOCKS 10
+#define SPEED 100
+
+#define BRICK_VAL
+#define WOOD_VAL
+#define ALUMINUM_VAL
+#define STEEL_VAL
+
+int block_count = 10;
+int blocks[10];
+int nextBlockVal;
+
 void setup() {
 	pinMode(TOP_LEFT, OUTPUT);
 	pinMode(TOP_RIGHT, OUTPUT);
@@ -13,7 +25,48 @@ void setup() {
 }
 
 void loop() {
-	forward(128);
+	while (block_count < NUM_BLOCKS) {
+		forward(SPEED);
+		while (not isBlock());
+		block_count++;
+		blocks[block_count] = blockVal();
+	}
+	calcNextBlock();
+}
+
+void calcNextBlock(){
+	for (int i = 0; i < 10; ++i)
+		if (blocks[i] >= BRICK_VAL) {
+			nextBlockVal = BRICK_VAL;
+			return;
+		}
+	}
+	for (int i = 0; i < 10; ++i)
+		if (blocks[i] >= WOOD_VAL) {
+			nextBlockVal = WOOD_VAL;
+			return;
+		}
+	}
+	for (int i = 0; i < 10; ++i)
+		if (blocks[i] >= ALUMINUM_VAL) {
+			nextBlockVal = ALUMINUM_VAL;
+			return;
+		}
+	}
+	for (int i = 0; i < 10; ++i)
+		if (blocks[i] >= STEEL_VAL) {
+			nextBlockVal = STEEL_VAL;
+			return;
+		}
+	}
+}
+
+// returns true if we are on top of a block
+bool isBlock() {
+}
+
+// reads the optical measurement of block
+int blockVal() {
 }
 
 void forward(int speed) {
